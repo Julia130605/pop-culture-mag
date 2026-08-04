d4d3213c0e127090e7b0459d0c6cf5117fff6721
Julia <a2148812733@gmail.com>
2026-08-04 21:13:05 +0800

    update hero image and mobile layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <img
   src={heroImage}
   alt="Pop music live performance"
-  className="absolute inset-0 h-full w-full object-cover object-[65%_center] md:object-center"
+  className="absolute inset-0 h-full w-full object-cover object-[70%_center] md:object-center"
 />
 
 <div className="absolute inset-0 bg-black/55" />
