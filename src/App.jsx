1d942eaeb2b783b3710aa77dfcb951b713a99a72
Julia <a2148812733@gmail.com>
2026-08-06 11:48:47 +0800

    fix router and restore homepage sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,96 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 
 import Hero from "./components/Hero";
-
 import EditorNote from "./components/EditorNote";
-
 import SocialFollow from "./components/SocialFollow";
-
 import LatestNews from "./components/LatestNews";
-
 import MusicPicks from "./components/MusicPicks";
-
 import ArtistSpotlight from "./components/ArtistSpotlight";
-
 import VideoMoments from "./components/VideoMoments";
-
 import Footer from "./components/Footer";
 
 
+// 首页
+function Home() {
 
-function Home(){
+  return (
 
+    <>
+      <Hero />
 
-return (
+      <EditorNote />
 
-<>
+      <SocialFollow />
 
+      <LatestNews />
 
-<Hero />
+      <MusicPicks />
 
+      <ArtistSpotlight />
 
-<EditorNote />
+      <VideoMoments />
 
+      <Footer />
+    </>
 
-<SocialFollow />
+  );
 
-
-<LatestNews />
-
-
-<MusicPicks />
-
-
-<ArtistSpotlight />
-
-
-<VideoMoments />
+}
 
 
-<Footer />
+// Artist 页面（先占位）
+function Artists(){
 
+  return (
 
-</>
+    <div className="min-h-screen bg-white">
 
-)
+      <h1 className="text-6xl font-serif p-20">
+        Artists
+      </h1>
 
+    </div>
+
+  )
 
 }
 
 
 
-
-
-function App() {
-
+function App(){
 
 return (
 
-<>
+<BrowserRouter>
 
 
 <Navbar />
 
 
-<Home />
+<Routes>
 
 
-</>
+<Route 
+path="/" 
+element={<Home />}
+/>
 
-);
 
+<Route 
+path="/artists" 
+element={<Artists />}
+/>
+
+
+</Routes>
+
+
+</BrowserRouter>
+
+
+)
 
 }
 
